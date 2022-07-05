49ece4f50afbe15b9f82a9fadefaeb8b5ee55ae1
liuzhidi <462311233@qq.com>
2022-07-05 12:06:41 +0800

    返回首页组件

---
+++ src/components/Back.vue
@@ -0,0 +1,21 @@
+<template>
+  <router-link to="/"> <div class="return">返回首页</div></router-link>
+</template>
+
+<script>
+export default {
+  name: 'Back'
+}
+</script>
+<style scoped lang="less">
+.return {
+  position: fixed;
+  top: 0.5rem;
+  right: 0.3rem;
+  padding: 0.1rem 0.3rem;
+  background-color: rgba(51, 51, 51, 0.381);
+  color: #fff;
+  font-size: 0.25rem;
+  border-radius: 00.2rem;
+}
+</style>
