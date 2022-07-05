db5c8ddc9fc9e687d29f31bce1b3f57b9fb8eea9
liuzhidi <462311233@qq.com>
2022-07-05 12:08:38 +0800

    核算预约组件

---
+++ src/views/Hesuan/Hesuan.vue
@@ -0,0 +1,15 @@
+<template>
+  <img src="@/assets/images/hesuan.jpg" alt="" />
+  <Back></Back>
+</template>
+
+<script>
+import Back from '@/components/Back.vue'
+export default {
+  name: 'Hesuan',
+  components: {
+    Back
+  }
+}
+</script>
+<style scoped lang="less"></style>
