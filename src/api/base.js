36d998d3aa49aad5fb46708c1de704752c838c3d
liuzhidi <462311233@qq.com>
2022-07-07 21:53:48 +0800

    api请求

--- src/api/base.js
+++ src/api/base.js
@@ -1,8 +1,24 @@
 //接口地址
+
 const base = {
   //域名
   host: 'http://api.tianapi.com',
   //国内疫情路径
-  covInfo: '/ncov/index?key=02e5d66193b75d88947a81ebd987264c'
+  covInfo: '/ncov/index?key=02e5d66193b75d88947a81ebd987264c',
+  //域名
+  cnHost: '',
+  //国内疫情路径
+  chinaData: '/g2/getOnsInfo?name=disease_h5',
+  wHost:
+    'https://api.inews.qq.com/newsqa/v1/automation/modules/list?modules=FAutoGlobalStatis,FAutoContinentStatis,FAutoGlobalDailyList,FAutoCountryConfirmAdd',
+  img: 'http://iwenwiki.com/wapicovid19/ncovimg.php',
+  citys:
+    'http://iwenwiki.com/wapicovid19/query.php?key=171e165a7d991c5f6ecd5194c54778ef',
+  travel:
+    'http://iwenwiki.com/wapicovid19/citys.php?key=171e165a7d991c5f6ecd5194c54778ef',
+  cityCode: '/foo/aj/getCityCode'
 }
+
 export default base
+
+// 'https://api.inews.qq.com/newsqa/v1/automation/modules/list?modules=FAutoGlobalStatis,FAutoContinentStatis,FAutoGlobalDailyList,FAutoCountryConfirmAdd',
--- src/api/index.js
+++ src/api/index.js
@@ -6,6 +6,24 @@ const api = {
   //国内疫情数据接口
   getCovInfo() {
     return axios.get(base.host + base.covInfo)
+  },
+  getChinaInfo() {
+    return axios.get(base.cnHost)
+  },
+  getWorldInfo() {
+    return axios.get(base.wHost)
+  },
+  getImg() {
+    return axios.get(base.img)
+  },
+  getCitys(params) {
+    return axios.get(base.citys, { params })
+  },
+  getTravel() {
+    return axios.get(base.travel)
+  },
+  getCode() {
+    return axios.get(base.cityCode)
   }
 }
 export default api
