af82e131e211095e18753a24f5fdacca3a8518b7
liuzhidi <462311233@qq.com>
2022-07-07 21:54:05 +0800

    路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,54 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 
-const home = () => import('@/views/Home.vue')
+import home from '@/views/Home/Home.vue'
+const hesuan = () => import('@/views/Hesuan/Hesuan.vue')
+const area = () => import('@/views/Area/Area.vue')
+const wuzi = () => import('@/views/Wuzi/Wuzi.vue')
+const travel = () => import('@/views/Travel/Travel.vue')
+const select = () => import('@/views/Select/Select.vue')
 const routes = [
   {
     path: '/',
+    name: 'home',
     component: home
+  },
+  {
+    path: '/hesuan',
+    name: 'hesuan',
+    component: hesuan
+  },
+  {
+    path: '/area',
+    name: 'area',
+    component: area
+  },
+  {
+    path: '/wuzi',
+    name: 'wuzi',
+    component: wuzi
+  },
+  {
+    path: '/travel',
+    name: 'travel',
+    component: travel
+  },
+  {
+    path: '/select',
+    name: 'select',
+    component: select
   }
 ]
 
 const router = createRouter({
   history: createWebHashHistory(),
-  routes
+  routes,
+  //切换路由时页面回到顶部
+  scrollBehavior() {
+    //vue2.0
+    // return { x: 0, y: 0 }
+    //vue3.0
+    return { left: 0, top: 0 }
+  }
 })
 
 export default router
